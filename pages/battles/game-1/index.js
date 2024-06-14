a5f10fc8a2fce788bcc8e40f3e2b9f4a37229a60
gautamgaddam <gautammaddyson@gmail.com>
2024-06-14 23:52:48 +0530

    FIX | projects

--- pages/battles/game-1/index.js
+++ pages/battles/game-1/index.js
@@ -5,7 +5,7 @@ import { Typography } from "@mui/material";
 
 const Game1 = () => {
   const circlesRef = useRef([]);
-
+  const projectsRef = useRef([]);
   const colors = [
     "pink",
     "red",
@@ -16,11 +16,21 @@ const Game1 = () => {
     "purple",
     "teal",
   ];
-
+  const projects = ["project1", "project2", "project3", "project4"];
   useEffect(() => {
     circlesRef.current.forEach((circle, index) => {
       console.log({ x: index, ...(index > 0 && { delay: index / 10 + 0.01 }) });
       gsap.from(circle, {
+        duration: 1,
+        opacity: 1,
+        y: 40,
+        ease: Power3.easeOut,
+        ...(index > 0 && { delay: index / 10 + 0.01 }), // delay based on index
+      });
+    });
+    projectsRef.current.forEach((project, index) => {
+      console.log({ x: index, ...(index > 0 && { delay: index / 10 + 0.01 }) });
+      gsap.from(project, {
         duration: 1,
         opacity: 1,
         y: 40,
@@ -40,14 +50,32 @@ const Game1 = () => {
       >
         Projects()
       </Typography>
-
       <Typography
-        variant="h3"
+        variant="h4"
         align="left"
         gutterBottom
-        className={styles.formTitle}
+        className={styles.formSubTitle}
       >
-        Corporate()
+        Personal
+      </Typography>
+      <div className={styles.circleContainer}>
+        {projects.map((color, index) => (
+          <div
+            key={index}
+            ref={(el) => {
+              projectsRef.current[index] = el;
+            }}
+            className={`${styles.circle} ${styles[color]}`}
+          />
+        ))}
+      </div>
+      <Typography
+        variant="h4"
+        align="left"
+        gutterBottom
+        className={styles.formSubTitle}
+      >
+        Corporate
       </Typography>
       <div className={styles.circleContainer}>
         {colors.map((color, index) => (
